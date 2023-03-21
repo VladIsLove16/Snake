86f031621ae3cc3ba63f60839f84614773192c74
VladIsLove16 <zalezshi@gmail.com>
2023-03-21 13:55:50 +0300

    1

---
+++ SnakeByGPT/SnakeByGPT.cpp
@@ -0,0 +1,182 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+using namespace std;
+
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+int gameSpeed;
+
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+#define KEY_UP 72
+#define KEY_DOWN 80
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+    gameSpeed = 5;
+}
+
+void Draw()
+{
+    system("cls"); // очистить экран
+
+    // нарисовать верхнюю границу
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    // нарисовать поля
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    // нарисовать нижнюю границу
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    // вывести счет
+    cout << "Score: " << score << "    GameSpeed: "<<gameSpeed<< endl;
+}
+
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            if (dir != RIGHT)dir = LEFT;
+            break;
+        case 'd':
+            if (dir != LEFT)dir = RIGHT;
+            break;
+        case 'w':
+            if (dir != DOWN) dir = UP;
+            break;
+        case 's':
+            if (dir != UP)dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        case KEY_UP:
+         if(gameSpeed<20)gameSpeed++;
+            break;
+        case KEY_DOWN:
+            if (gameSpeed > 1)gameSpeed--;
+            break;
+        }
+    }
+}
+
+void Logic()
+{
+    // сохранить хвост змейки
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    // движение головы змейки
+    switch (dir)
+        {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+        }
+
+        // проверка на выход за границы
+            if (x > width || x < 0 || y > height || y < 0)
+                gameOver = true;
+
+        // проверка на съедание фрукта
+        if (x == fruitX && y == fruitY)
+        {
+            score += 10;
+            fruitX = rand() % width;
+            fruitY = rand() % height;
+            nTail++;
+        }
+
+        // проверка на съедание хвоста
+        for (int i = 0; i < nTail; i++)
+        {
+            if (tailX[i] == x && tailY[i] == y)
+                gameOver = true;
+        }
+}
+
+int main()
+{
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+        Sleep(210-10*gameSpeed); // замедление движения змейкиadsa
+    }
+
+    return 0;
+}
